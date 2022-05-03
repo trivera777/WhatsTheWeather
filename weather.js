ad1b7da67fb062e6f4092bd2838bd435b72e40ae
Tifanny Rivera <trivera51580@gmail.com>
2022-05-03 12:07:46 -0600

    working date interval

--- weather.js
+++ weather.js
@@ -5,3 +5,12 @@ const timezone = document.getElementById('timeZone')
 const countryEl = document.getElementById('country')
 const weatherForecastEl = document.getElementById('weatherForecast')
 const currentTempEl = document.getElementById('currentTemp')
+
+setInterval(() => {
+    const time = new Date()
+    const month = time.getMonth()
+    const date = time .getDate()
+    const day = time.getDay()
+    const hour = time.getHours()
+    const minutes = time.getMinutes()
+}, 1000);
