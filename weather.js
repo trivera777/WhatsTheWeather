ff68d90d2819f86af466b3edc19cb1f9ea96269d
Tifanny Rivera <trivera51580@gmail.com>
2022-12-30 10:54:08 -0700

    api key works, current weather container width adjusted

--- weather.js
+++ weather.js
@@ -30,7 +30,7 @@ const months = [
   "Dec",
 ];
 
-const API_key = "b7aa93d2653535fd86c395597ddcf6c8";
+// const API_key = "b7aa93d2653535fd86c395597ddcf6c8";
 
 setInterval(() => {
   const time = new Date();
@@ -42,7 +42,8 @@ setInterval(() => {
   const hours24format = hour >= 13 ? hour % 12 : hour;
   const amPm = hour >= 12 ? "PM" : "AM";
 
-  timeEl.innerHTML = hours24format + ":" + minutes + `<span id="amPm">${amPm}</span>`;
+  timeEl.innerHTML =
+    hours24format + ":" + minutes + `<span id="amPm">${amPm}</span>`;
 
   dateEl.innerHTML = days[day] + ", " + months[month] + " " + date;
 }, 1000);
@@ -54,37 +55,34 @@ function getWeatherData() {
     let { latitude, longitude } = success.coords;
 
     fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&unit=imperial&appid=${API_key}${API_key}`
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${API_key}`
     )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        // showWeatherData(data);
+        showWeatherData(data);
       });
   });
 }
-// function showWeatherData(data) {
-//   let { humidity, pressure, sunrise, sunset, windSpeed } = data.current;
+function showWeatherData(data) {
+  let { temp, humidity, clouds, wind_speed } = data.current;
 
-//   currentWeatherItemsEl.innerHTML = ` <div class="weatherItem">
-//         <div>Humidity</div>
-//         <div>${humidity}</div>
-//     </div>
-//     <div class="weatherItem">
-//         <div>Pressure</div>
-//         <div>${pressure}</div>
-//     </div>
-//     <div class="weatherItem">
-//         <div>Wind Speed</div>
-//         <div>${windSpeed}</div>
-//     </div>
-//     <div class="weatherItem">
-//         <div>Sunrise</div>
-//         <div>${window.moment(sunrise).format("HH:mm a")}</div>
-//     </div>
-//     <div class="weatherItem">
-//         <div>Sunset</div>
-//         <div>${window.moment(sunset).format("HH:mm a")}</div>
-//     </div>
-//     `;
-// }
+  currentWeatherItemsEl.innerHTML = ` 
+    <div class="weatherItem">
+      <div>Temp</div>
+      <div>${temp}°F</div>
+    </div>
+    <div class="weatherItem">
+      <div>Humidity</div>
+      <div>${humidity}%</div>
+    </div>
+    <div class="weatherItem">
+     <div>Cloudiness</div>
+     <div>${clouds}%</div>
+    </div>
+    <div class="weatherItem">
+      <div>Wind Speed</div>
+      <div>${wind_speed}mph</div>
+    </div>
+  `;
+}
