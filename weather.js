229d12764b0e83bf48d2e68538ef15c9a1af29a1
Tifanny Rivera <trivera51580@gmail.com>
2023-01-04 19:27:52 -0700

    hid loading page

--- weather.js
+++ weather.js
@@ -14,20 +14,28 @@ let weather = {
       const {icon, description} = data.weather[0]
       const {temp, humidity} = data.main
       const {speed} = data.wind
-      console.log(name, icon, description, humidity, temp, speed)
       document.querySelector(".city").innerText = name
       document.querySelector(".icon").src = `https://openweathermap.org/img/wn/` + icon + `.png`
       document.querySelector(".description").innerText = description
       document.querySelector(".temp").innerText = temp + `°F`
       document.querySelector(".humidity").innerText = `Humidity ` + humidity + `%`
       document.querySelector(".wind").innerText = `Wind speed: ` + speed + ` mph`
+      document.querySelector(".weather").classList.remove("loading")
     },
     search: function() {
       this.fetchWeather(document.querySelector(".searchbar").value)
     }
   }
-
+// event listener for the 'click' on search button
 document.querySelector(".searchbutton")
 .addEventListener("click", function(){
   weather.search()
 })
+// event listener for the 'enter' key
+document.querySelector(".searchbar").addEventListener("keyup", function(event) {
+  if(event.key == "Enter"){
+    weather.search()
+  }
+})
+
+weather.fetchWeather("Denver")
